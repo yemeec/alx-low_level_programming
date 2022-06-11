69f8511fb8c97e1f8fd5d38fc8a5b72348705003
yemeec <56555943+yemeec@users.noreply.github.com>
2022-06-11 08:45:03 +0100

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ * #ifndef MAIN_H & #define MAIN_H : to remove multiple declearation
+ * 
+ */
+
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
